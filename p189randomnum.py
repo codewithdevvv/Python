debc1f4e70bcf79f24a4fd4da3927bb78afa8895
codewithdevvv <devrajmistry26@gmail.com>
2024-09-24 18:08:07 +0530

    Basic

---
+++ p189randomnum.py
@@ -0,0 +1,11 @@
+import random
+num=0
+x=random.randint(1,50)
+c=0
+while x!=num:
+    num=int(input("Enter your number:-"))
+    print("again")
+    c=c+1
+print("you took",c,"try to win this game!")
+ 
+   
